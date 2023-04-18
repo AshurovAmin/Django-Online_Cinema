ef7825273ceb2c021c2dd7b4f2da9960ff3d17ec
Janna-git <zhannat.shayakunova@mail.ru>
2023-04-18 14:03:14 +0600

    Added Category, Movie models

--- online_cinema/models.py
+++ online_cinema/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.TextField(max_length=255)
+
+
+class Movie(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.TextField(max_length=255)
+    year = models.DateField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField()
+    video = models.FileField()
+
+
